2b080b3865b3f8d23c20cb51c8239d5f3fe998ef
Rebeca Baruch <71520936+RebecaBaruch@users.noreply.github.com>
2024-10-08 20:00:08 -0300

    fix: remove unnecessary component

--- src/pages/login/view/login.view.tsx
+++ src/pages/login/view/login.view.tsx
@@ -4,7 +4,7 @@ import {
   Wrapper,
 } from '../components/login-template/index.component';
 import InputBox from '../../../components/input-box/input-box.component';
-import { PasswordField, Stack } from '@telefonica/mistica';
+import { Stack } from '@telefonica/mistica';
 import Input from 'src/components/input/input.component';
 import { LoginProps } from '../types';
 
@@ -22,16 +22,6 @@ export default function Login({ email }: LoginProps) {
                 type="text"
                 value={email}
                 onChange={() => console.log('oi')}
-              />
-            }
-          />
-          <InputBox
-            title="Senha"
-            input={
-              <PasswordField
-                name="password"
-                label=""
-                placeholder="Digite a sua senha"
               />
             }
           />
