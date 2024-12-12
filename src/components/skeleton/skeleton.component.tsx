a035319678a2dc59c91e433d468280cabf93a86a
Rebeca Baruch <71520936+RebecaBaruch@users.noreply.github.com>
2024-12-12 16:09:16 -0300

    fix: skeleton component props

--- src/components/skeleton/skeleton.component.tsx
+++ src/components/skeleton/skeleton.component.tsx
@@ -2,7 +2,9 @@ import React from 'react';
 import { Box, FadeIn, SkeletonRectangle, Stack } from '@telefonica/mistica';
 import { SkeletonProps } from './types';
 import { v4 as uuidv4 } from 'uuid';
+import { ByBreakpoint } from '@telefonica/mistica/dist/utils/types';
 
+type PadSize = 0 | 2 | 4 | 8 | 12 | 16 | 20 | 24 | 32 | 40 | 48 | 56 | 64 | 72 | 80;
 export default function SkeletonComponent({
   repeatSkeleton,
   heightSkeleton,
@@ -10,7 +12,10 @@ export default function SkeletonComponent({
   paddingXSkeleton,
 }: SkeletonProps) {
   return (
-    <Box paddingY={paddingYSkeleton} paddingX={paddingXSkeleton}>
+    <Box
+      paddingY={paddingYSkeleton as ByBreakpoint<PadSize>}
+      paddingX={paddingXSkeleton as ByBreakpoint<PadSize>}
+    >
       <FadeIn duration="0.25s">
         <Stack space={8}>
           {Array.from({ length: repeatSkeleton }).map(() => (
--- src/components/skeleton/types.ts
+++ src/components/skeleton/types.ts
@@ -1,6 +1,10 @@
+import { ByBreakpoint } from "@telefonica/mistica/dist/utils/types";
+
+type PadSize = 0 | 2 | 4 | 8 | 12 | 16 | 20 | 24 | 32 | 40 | 48 | 56 | 64 | 72 | 80;
+
 export interface SkeletonProps {
   repeatSkeleton: number;
   heightSkeleton: number;
-  paddingXSkeleton?: number;
-  paddingYSkeleton?: number;
+  paddingYSkeleton?: ByBreakpoint<PadSize>;
+  paddingXSkeleton?: ByBreakpoint<PadSize>;
 }
