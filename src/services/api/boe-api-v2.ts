b06048370a7797c391d9d5533ff330cffdffda83
Rebeca Baruch <71520936+RebecaBaruch@users.noreply.github.com>
2024-12-12 16:00:12 -0300

    feat: BoeApiv2 services

--- src/services/api/boe-api-v2.ts
+++ src/services/api/boe-api-v2.ts
@@ -1,73 +1,95 @@
+import axios from 'axios';
+
 const API_URL = process.env.NEXT_PUBLIC_API_URL;
+interface CustomError extends Error {
+  statusCode?: number;
+}
 
-const BoeApiV2 = {
-  user: {
-    signUp: async (userData) => {
-      const response = await fetch(`${API_URL}/user/signup`, {
+const boeApiV2 = {
+  signIn: async (email: string, password: string) => {
+    try {
+      const response = await axios.post(`${API_URL}/api/user/signin`, {
+        email,
+        password,
+      });
+
+      return response.data;
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        const statusCode = error.response?.status;
+        const errorMessage =
+          error.response?.data?.message ||
+          'Falha na autenticação. Tente novamente.';
+
+        const axiosError: CustomError = new Error(errorMessage);
+        axiosError.statusCode = statusCode;
+
+        throw axiosError;
+      }
+
+      throw new Error('Erro desconhecido');
+    }
+  },
+
+  userSignUp: async (
+    name: string,
+    email: string,
+    phone: string,
+    password: string,
+  ) => {
+    try {
+      const response = await fetch(`${API_URL}/api/user/signup`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify(userData),
+        body: JSON.stringify({ name, email, phone, password }),
       });
 
-      if (!response.ok) {
-        throw new Error('Error creating user');
-      }
+      const data = await response.json();
+      console.log('Response:', data);
+      return data;
+    } catch (error) {
+      console.error('SignIn Error:', error);
+      throw error;
+    }
+  },
 
-      return response.json();
-    },
-
-    signIn: async (credentials) => {
-      const response = await fetch(`${API_URL}/user/signin`, {
+  farmRegister: async (
+    propertyName: string,
+    propertyPhone: string,
+    taxId: string,
+    zipCode: string,
+    state: string,
+    city: string,
+    address: string,
+  ) => {
+    try {
+      const response = await fetch(`${API_URL}/api/farm/register`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
+          Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY3NWEyMmM1NmMxZDM1N2MyNDQyM2U2NSIsImlhdCI6MTczMzk2MTg3MiwiZXhwIjoxNzMzOTY1NDcyfQ.iw-KJ-ydLJspVhp4uQPoyR_zOsAPHVsyqoepu0Ouav8`,
         },
-        body: JSON.stringify(credentials),
+        body: JSON.stringify({
+          name: propertyName,
+          tax_id: taxId,
+          phone: propertyPhone,
+          zip_code: zipCode,
+          state,
+          city,
+          address,
+        }),
       });
 
-      if (!response.ok) {
-        throw new Error('Error logging in');
-      }
-
-      return response.json();
-    },
-
-    update: async (userId, userData) => {
-      const response = await fetch(`${API_URL}/user/update`, {
-        method: 'PUT',
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: `Bearer ${localStorage.getItem('jwt')}`,
-          userId: userId,
-        },
-        body: JSON.stringify(userData),
-      });
-
-      if (!response.ok) {
-        throw new Error('Error updating user');
-      }
-
-      return response.json();
-    },
-
-    delete: async (userId) => {
-      const response = await fetch(`${API_URL}/user/delete`, {
-        method: 'DELETE',
-        headers: {
-          Authorization: `Bearer ${localStorage.getItem('jwt')}`,
-          userId: userId,
-        },
-      });
-
-      if (!response.ok) {
-        throw new Error('Error deleting user');
-      }
-
-      return response.json();
-    },
+      const data = await response.json();
+      console.log('Response:', data);
+      return data;
+    } catch (error) {
+      console.error('Farm Register Error:', error);
+      throw error;
+    }
   },
 };
 
-export default BoeApiV2;
+export default boeApiV2;
