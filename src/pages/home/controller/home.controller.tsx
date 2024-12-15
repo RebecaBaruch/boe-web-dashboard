3392b01c018fc098f39116f1b15f22a559fa2d89
Rebeca Baruch <71520936+RebecaBaruch@users.noreply.github.com>
2024-12-15 18:26:10 -0300

    fix: login-context

--- src/pages/home/controller/home.controller.tsx
+++ src/pages/home/controller/home.controller.tsx
@@ -3,17 +3,21 @@ import Home from '../view/home.view';
 import { getGreeting } from 'utils/date-utils';
 import { ThemeContextProvider } from '@telefonica/mistica';
 import { theme } from '../../../config/theme';
+import { useLoginContext } from '../../../context/login-context';
 
 export default function HomeController() {
+  const { userData } = useLoginContext();
+  const [userName, setUserName] = React.useState('');
   const [greetingText, setGreetingText] = React.useState('');
 
   React.useEffect(() => {
+    setUserName(userData.name);
     setGreetingText(getGreeting());
-  }, []);
+  }, [userData]);
 
   return (
     <ThemeContextProvider theme={theme}>
-      <Home greetingText={greetingText} userName={'Rebeca'} />;
+      <Home greetingText={greetingText} userName={userName} />;
     </ThemeContextProvider>
   );
 }
