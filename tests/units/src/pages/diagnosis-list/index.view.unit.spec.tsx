0c9b0fcf724d5663ba7c16086d0eb50cb726861c
Rebeca Baruch <71520936+RebecaBaruch@users.noreply.github.com>
2024-12-15 17:57:40 -0300

    test: update tests

--- tests/units/src/pages/diagnosis-list/index.view.unit.spec.tsx
+++ tests/units/src/pages/diagnosis-list/index.view.unit.spec.tsx
@@ -5,8 +5,12 @@ import { renderWithTheme } from '../../../../../src/utils/render-with-theme';
 import { diagnosisMockData } from '../../../../../src/pages/diagnosis-list/mock/table-data';
 import { diagnosisListColumns } from '../../../../../src/config/constants/column-headers';
 
+jest.mock('../../../../../src/services/api/boe-api-v2', () => ({
+  getAnalysisHistory: jest.fn(() => Promise.resolve(diagnosisMockData)),
+}));
+
 describe('DiagnosisList', () => {
-  it('should render DiagnosisList with expected headers', () => {
+  it('should render DiagnosisList with expected headers', async () => {
     renderWithTheme(
       <DiagnosisList
         diagnosisData={diagnosisMockData}
@@ -29,5 +33,28 @@ describe('DiagnosisList', () => {
     expect(screen.getByText('Autor da análise')).toBeInTheDocument();
     expect(screen.getByText('Data')).toBeInTheDocument();
     expect(screen.getByText('Ação')).toBeInTheDocument();
+  });
+
+  it('should fetch and render diagnosis data', async () => {
+    renderWithTheme(
+      <DiagnosisList
+        diagnosisData={diagnosisMockData}
+        columnsHeader={diagnosisListColumns}
+        currentPage="1"
+        lastPage="3"
+        prevPage={jest.fn()}
+        nextPage={jest.fn()}
+        toggleSelectRow={jest.fn()}
+        toggleSelectAll={jest.fn()}
+        onSelectMode={jest.fn()}
+        activeTab={'anality'}
+        setActiveTab={jest.fn()}
+      />,
+    );
+
+    // Espera pela renderização dos dados após a chamada da API
+    await screen.findByText(diagnosisMockData[0].animalId); // Verifique se o dado mockado é renderizado
+
+    expect(screen.getByText(diagnosisMockData[0].animalId)).toBeInTheDocument();
   });
 });
